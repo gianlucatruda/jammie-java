5c5ed1b4c528b78064eb4fedf71f988ef9754bc7
Gianluca Truda <gianlucatruda@gmail.com>
2017-02-27 13:15:20 +0200

    Added basic code to TextInputter.

---
+++ src/TextInputter.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * This is a general way to read in data from a text file line by line.
+ * Created by gianlucatruda on 27/02/2017.
+ */
+public class TextInputter {
+
+	public TextInputter() {
+
+	}
+
+	public ArrayList<String> getText(String textfile) {
+
+		ArrayList<String> outArray = new ArrayList<>();
+		try {
+			Scanner scanner = new Scanner(new File(textfile));
+			while (scanner.hasNextLine()) {
+				outArray.add(scanner.nextLine());
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		return outArray;
+	}
+
+
+}
